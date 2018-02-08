38fec7d1fcc64725a36eb906214f23225a8fb7da
Ian Brennan <ianbrennan@ian-playbook.attlocal.net>
2018-02-07 21:00:09 -0600

    logs!

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from gpiozero import LED
 import time
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+logging.debug("test")
 led = LED(17)
 
 def main():
+    logging.debug("sleeping")
     time.sleep(6)
-    print("turning led on")
+    logging.debug("turning led on")
     led.on()
+    return
 
 main()
     
