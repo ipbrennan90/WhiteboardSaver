a248b2de8b27e841d79cda88b244c61769766c35
Ian Brennan <ianbrennan@ian-playbook.attlocal.net>
2018-02-09 09:45:57 -0600

    twilio

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 from gpiozero import LED
 import time
 import logging
+from twilio.rest import Client
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 logging.debug("test")
 led = LED(17)
+account_sid = os.environ['TWILIO_ACCT']
+auth_token = os.environ['TWILIO_TOKEN']
+twilio_client = Client(account_sid, auth_token)
 
+
+def message():
+    try:
+        body = twilio_client.messages.create(
+            "+13033190247",
+            body="I turned on!",
+            from_="+17205482762",
+            status_callback="https://requestb.in/12hql6p1"
+        )
+    except IOError as e:
+        print 'ERROR: {}'.format(e)
+    finally:
+        print("DID IT")
+        
 def main():
     logging.debug("start")
     led.off()
